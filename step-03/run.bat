878efc598bc6d608e4630ccd75a5370864829e2f
Fractal2017 <29259883+Fractal2017@users.noreply.github.com>
2020-01-13 01:03:21 -0500

    Delete run.bat

--- step-03/run.bat
+++
@@ -1,9 +0,0 @@
-ECHO OFF
-
-ECHO Docker volume: Share content between containers
-ECHO create a common volume (mount point) by using parameter -v
-ECHO Extract a rar file
-
-docker run --rm -v %CD%:/Files maxcnunes/unrar unrar x -r Trunck.rar
-
-DEMO:
