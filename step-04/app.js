e44e5970ab7edead39b9e05c1cdfc25ed4553e69
Fractal2017 <29259883+Fractal2017@users.noreply.github.com>
2020-01-13 01:14:01 -0500

    Delete app.js

--- step-04/app.js
+++
@@ -1,8 +0,0 @@
-var http=require('http');
-
-http.createServer(function(req,res){
-	res.writeHead(200,{'Content-Type': 'text/plain'});
-	res.end('Hello Datavail team');
-}).listen(3000);
-
-console.log('SERVER is UP and Running');
